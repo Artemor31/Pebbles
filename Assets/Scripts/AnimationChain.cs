2d3a4ab64c46b3e6fa05ea09ee0aff5b9c9c9277
Artyom <bogunov_aa@edu.bstu.ru>
2022-02-19 09:58:54 +0300

    fix: start animations

---
+++ Assets/Scripts/AnimationChain.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Gameplay
+{
+    public class AnimationChain : MonoBehaviour
+    {
+        [SerializeField] private Animator _nextAnimator;
+
+        public void StartNext()
+        {
+            _nextAnimator.Play("start");
+        }
+    }
+}
---
+++ Assets/Scripts/AnimatorScheduler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class AnimatorScheduler : MonoBehaviour
+    {
+        [SerializeField] private Animator _pebbles;
+        [SerializeField] private Animator _handIcon;
+
+        public void StartChain()
+        {
+            _pebbles.Play("start");
+            _handIcon.Play("start");
+        }
+
+        private void Start()
+        {
+            StartChain();
+        }
+    }
+}
