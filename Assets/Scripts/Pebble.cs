07437112a18e68e36be30031fbc18fd3a685c112
Артем Богунов <bogunov_aa@edu.bstu.ru>
2022-02-19 21:47:18 +0300

    fix: pebble

--- Assets/Scripts/Pebble.cs
+++ Assets/Scripts/Pebble.cs
@@ -1,7 +1,25 @@
-﻿namespace Gameplay
+﻿using System;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace Gameplay
 {
-    public class Pebble
+    public class Pebble : MonoBehaviour, IPointerDownHandler
     {
-        
+        public void OnPointerDown(PointerEventData eventData)
+        {
+            transform.position = Pointer.Instance.transform.position;
+        }
+    }
+
+    public class Pointer : MonoBehaviour
+    {
+        public static Pointer Instance;
+
+        private void Awake()
+        {
+            Instance = FindObjectOfType<Pointer>();
+        }
     }
 }
