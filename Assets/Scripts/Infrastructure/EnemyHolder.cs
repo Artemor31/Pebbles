ff2ab5d5582f362d171c98ec12155277e1a7c985
Artyom <bogunov_aa@edu.bstu.ru>
2022-04-19 10:28:21 +0300

    removing player from singletons

--- Assets/Scripts/Infrastructure/EnemyHolder.cs
+++ Assets/Scripts/Infrastructure/EnemyHolder.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using AnimationSchemas;
+using StateMachine;
 using UnityEngine;
 using Utils;
 using Zenject;
@@ -11,69 +12,22 @@ namespace Infrastructure
 {
     public class EnemyHolder : MonoBehaviour
     {
-        public Action<int> ValuePicked;
-        public Action Picked;
-        
-        public int PebblesPicked => _pebblesPicked;
-        public int PickedCard => _pickedCard;
+        public int PebblesPicked { get; private set; }
+        public int PickedCard { get; private set; }
+        public int PebblesLeft { get; set; } = 3;
 
         [SerializeField] private List<SpriteRenderer> _pebbleInHand;
         [SerializeField] private AnimatorScheduler _animator;
         [SerializeField] private GameTimer _timer;
-        private int _pebblesLeft = 3;
-        private int _pickedCard;
-        private int _pebblesPicked;       
-        private PlayerHolder _playerHolder;
         
+        private PlayerHolder _playerHolder;
+        private PebbleState _pebbleState;
+
         [Inject]
-        public void Constructor(PlayerHolder playerHolder)
+        public void Constructor(PlayerHolder playerHolder, PebbleState pebbleState)
         {
             _playerHolder = playerHolder;
-        }
-
-        private void HidePebbles()
-        {
-            var max = _pebblesLeft + 1;
-            _pebblesPicked = Random.Range(0, max);
-        }
-
-        private void ChooseCard(bool firstTurn)
-        {
-            if (firstTurn)
-            {
-                var maxInclusive = _pebblesPicked + _playerHolder.PebblesLeft + 1;
-                _pickedCard = Random.Range(_pebblesPicked, maxInclusive);
-            }
-            else
-            {
-                var stMax = _playerHolder.PebblesLeft;
-
-                var min = _playerHolder.CardValue <= stMax
-                           ? _pebblesPicked
-                           : _pebblesPicked + (_playerHolder.CardValue - stMax);
-
-                var max = _playerHolder.CardValue > 0
-                           ? _playerHolder.PebblesLeft + _pebblesPicked
-                           : _playerHolder.CardValue + _pebblesPicked;
-
-                _pickedCard = Random.Range(min, max + 1);
-
-                if (_pickedCard == _playerHolder.CardValue)
-                {
-                    _pickedCard = _pickedCard == 6 
-                                ? _pickedCard - 1 
-                                : _pebblesPicked + 1;
-                }
-            }
-        }
-
-        private void SetupPebblesInHand()
-        {
-            foreach (var spriteRenderer in _pebbleInHand) 
-                spriteRenderer.enabled = false;
-
-            for (var i = 0; i < PebblesPicked; i++) 
-                _pebbleInHand[i].enabled = true;
+            _pebbleState = pebbleState;
         }
 
         public void StartChoosePebbles()
@@ -88,13 +42,52 @@ namespace Infrastructure
             yield return new WaitForSeconds(Random.Range(3, 8));
             _animator.ShowEnemyFist();
             _timer.StopAI();
+            _pebbleState.EnemyPickedPebbles();
         }
-        
-        public IEnumerator PickingCard()   
+
+        private void HidePebbles()
         {
-            var seconds = Random.Range(3, 5);
-            yield return new WaitForSeconds(seconds);
-            Picked?.Invoke();
+            var max = PebblesLeft + 1;
+            PebblesPicked = Random.Range(0, max);
+        }
+
+        private void ChooseCard(bool firstTurn)
+        {
+            if (firstTurn)
+            {
+                var maxInclusive = PebblesPicked + _playerHolder.PebblesLeft + 1;
+                PickedCard = Random.Range(PebblesPicked, maxInclusive);
+            }
+            else
+            {
+                var stMax = _playerHolder.PebblesLeft;
+
+                var min = _playerHolder.CardValue <= stMax
+                           ? PebblesPicked
+                           : PebblesPicked + (_playerHolder.CardValue - stMax);
+
+                var max = _playerHolder.CardValue > 0
+                           ? _playerHolder.PebblesLeft + PebblesPicked
+                           : _playerHolder.CardValue + PebblesPicked;
+
+                PickedCard = Random.Range(min, max + 1);
+
+                if (PickedCard == _playerHolder.CardValue)
+                {
+                    PickedCard = PickedCard == 6 
+                                ? PickedCard - 1 
+                                : PebblesPicked + 1;
+                }
+            }
+        }
+
+        private void SetupPebblesInHand()
+        {
+            foreach (var spriteRenderer in _pebbleInHand) 
+                spriteRenderer.enabled = false;
+
+            for (var i = 0; i < PebblesPicked; i++) 
+                _pebbleInHand[i].enabled = true;
         }
     }
 }
--- Assets/Scripts/Infrastructure/GameplayWrapper.cs
+++ Assets/Scripts/Infrastructure/GameplayWrapper.cs
@@ -1,36 +1,14 @@
 ﻿using System.Collections;
 using AnimationSchemas;
-using Cards;
 using UnityEngine;
-using Utils;
-using Zenject;
 
 namespace Infrastructure
 {
     public class GameplayWrapper : MonoBehaviour
     {
         [SerializeField] private AnimatorScheduler _animator;
-        [SerializeField] private GameTimer _timer;
         [SerializeField] private EnemyHolder _enemy;
         [SerializeField] private PlayerHolder _player;
-        
-        private void OnEnable()
-        {
-            _enemy.ValuePicked += AiPickedValue;
-            _player.PickedValue += PlayerPickedValue;
-        }
-
-
-        private void AiPickedValue(int value)
-        {
-            _timer.StopAI();
-        }
-
-        private void PlayerPickedValue(int value)
-        {
-            _player.CardValue = value;
-            _timer.StopPlayer();
-        }
 
         public IEnumerator ShowOff()
         {
